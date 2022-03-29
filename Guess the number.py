504f324b0f6228ee91fd57f61217829083d1086e
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-03-29 16:41:40 +0530

    Finally I made my first game!

--- Guess the number.py
+++ Guess the number.py
@@ -1,16 +1,25 @@
-number = 18
+number = 30
 guess = 1
-print("You have a total of 9 guesses!") 
+print("You have to guess a number between 1 to 100.\nYou have a total of 9 guesses!") 
 while(guess <= 9):
     guessed = int(input("Guess the number:\n"))
-    if guessed > 18:
+    if guessed > 30:
         print("Your number is greater than the number to be guessed!\n")
+        print(9 - guess, "number of guesses left")  
+        guess = guess + 1 
         continue
-    elif guessed < 18:
+    elif guessed < 30:
         print("Your number is smaller than the number to be guessed!\n")
+        print(9 - guess, "number of guesses left")   
+        guess = guess + 1
         continue
     else:
         print("Congratulations! You guessed the number!")
         print("You took", guess, "number of guesses to solve!")
+        guess = guess + 1
         break
-    print(9 - guess, "number of guesses left")   
+    
+if guess > 9:
+    print("GAME OVER")
+    
+    
