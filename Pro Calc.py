42a56a4394fdaf4d4ff1d58e28e28fb6e91d0873
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-04-04 20:05:13 +0530

    OPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP

--- Pro Calc.py
+++ Pro Calc.py
@@ -1,9 +1,10 @@
-num1 = float(input("Enter first number:\n"))
-num2 = float(input("Enter second number:\n"))
+a = 1
 
-choice = input("Choose opration\n 1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n 5. Square\n 6. Cube\n")
+while(a<2):
+    num1 = float(input("Enter first number:\n"))
+    num2 = float(input("Enter second number:\n"))
 
-while(True):
+    choice = input("Choose opration\n 1. Addition\n 2. Substraction\n 3. Multiplication\n 4. Division\n 5. Square\n 6. Cube\n")
     if choice == "1":
         print(num1, "+", num2, "=", num1 + num2)
 
@@ -20,16 +21,20 @@ while(True):
         print("The square of", num1, "is", num1 * num1)
         print("The square of", num2, "is", num2 * num2)
     
-    elif choice == "5":
+    elif choice == "6":
         print("The cube of", num1, "is", num1 * num1 * num1)
         print("The cube of", num2, "is", num2 * num2 * num2) 
     
     else:
         print("WRONG FORMATE")
-        continue
-    
-
   
+    a = a + 0
+    continue
+
+
+
+
+        
      
 
 
